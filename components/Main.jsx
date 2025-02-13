4374b25e2e82b582f6c57334cbd8f00ee54b36a0
Karan Singh <karansingh@Mac.lan>
2025-02-12 20:50:45 -0600

    Finished

---
+++ components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main(){
+    return(
+        <main>
+            <h1>Fun facts about React</h1>
+            <ul className="bullets">
+                <li>Was first released in 2013</li>
+                <li>Is maintaied by Meta</li>
+                <li>Is a lot of fun to learn</li>
+                <li>You can build your web and mobile apps with React</li>
+            </ul>
+        </main>
+        
+    )
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar(){
+    return(
+        <header>
+            <nav>
+                <img src="../src/react_logo.png" alt="React logo" />
+                <span className="heading" >ReactFacts</span>
+            </nav>
+        </header>
+        
+    )
+}
